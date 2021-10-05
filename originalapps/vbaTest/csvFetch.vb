5945edcedde05e108adaae49a2aa86990b04f88e
RK <r.r.k.k.9.14@gmail.com>
2021-10-05 23:06:16 +0900

    csv fetch vba revised

--- originalapps/vbaTest/csvFetch.vb
+++ originalapps/vbaTest/csvFetch.vb
@@ -51,14 +51,9 @@ Sub CsvToExcel()
         For i = LBound(tmp) To UBound(tmp)
             '前行と同日付なら追加
             If row_no <> 1 Then
-                If data(row_no - 2) = tmp(1) Then
-                    '番号は明細に追加しない
-                    If i <> 6 Then
-                        Sheets(1).Cells(Fcn, i + 1).Value = tmp(i)
-                    Else
-                        Sheets(1).Cells(12, 5) = "番号："
-                        Sheets(1).Cells(12, 6) = tmp(6)
-                    End If
+                If data(row_no - 2) = tmp(6) Then
+                    'Excelへの書込関数を呼び出す
+                    Call WriteToExcel(i, Fcn, tmp, index)
                 Else
                     '改シート
                     If Not added Then
@@ -66,40 +61,55 @@ Sub CsvToExcel()
                         Call AddStyles(Fcn)
                         ' シート追加
                         Worksheets.Add
-                        index = index + 1
                         added = True
                         row_no = 1
+                        index = 1
                         i = i - 1
                         Fcn = 2
                     End If
                 End If
             Else
-                '番号は明細に追加しない
-                If i <> 6 Then
-                    Sheets(1).Cells(Fcn, i + 1).Value = tmp(i)
-                Else
-                    Sheets(1).Cells(12, 5) = "番号："
-                    Sheets(1).Cells(12, 6) = tmp(6)
-                End If
+                'Excelへの書込関数を呼び出す
+                Call WriteToExcel(i, Fcn, tmp, index)
             End If
         Next i
         '要素追加管理
         If row_no = 1 Then
             ReDim data(1)
-            data(0) = tmp(1)
+            data(0) = tmp(6)
         Else
             ReDim Preserve data(UBound(data) + 1)
-            data(UBound(data) - 1) = tmp(1)
+            data(UBound(data) - 1) = tmp(6)
         End If
         added = False
         row_no = row_no + 1
-        Sheets(1).Name = "0000" + CStr(index)
+        'Sheets(1).Name = "0000" + CStr(index)
+        Sheets(1).Name = tmp(6)
+        index = index + 1 '行番号用
     Loop
     'ヘッダデータ取得、スタイル修正（関数呼び出し）
     Sheets(1).Cells(12, 5) = "番号："
     Sheets(1).Cells(12, 6) = tmp(6)
     Call AddStyles(Fcn + 1)
     Close #1
+End Sub
+
+
+'Excelへの書込関数
+Sub WriteToExcel(i As Long, Fcn As Long, tmp As Variant, index As Long)
+    With Sheets(1)
+        '番号は明細に追加しない
+        If i = 6 Then
+            Cells(12, 5) = "番号："
+            Cells(12, 6) = tmp(6)
+        '行番号はシートごとにリセットした値
+        ElseIf i = 0 Then
+            Cells(Fcn, i + 1).Value = index
+        'その他の値は順番に書き込んでいく
+        Else
+            Cells(Fcn, i + 1).Value = tmp(i)
+        End If
+    End With
 End Sub
 
 
@@ -120,7 +130,7 @@ Sub AddStyles(Fcn As Long)
         Range(Cells(2, 1), Cells(Fcn - 1, 6)).Borders(xlEdgeTop).LineStyle = xlContinuous
         Range(Cells(2, 1), Cells(Fcn - 1, 6)).Borders(xlInsideHorizontal).LineStyle = xlContinuous
         Range(Cells(Fcn, 5), Cells(Fcn, 6)).Borders(xlEdgeBottom).LineStyle = xlDouble
-        Range(Cells(2, 1), Cells(Fcn - 1, 6)).EntireRow.RowHeight = 16
+        Range(Cells(2, 1), Cells(Fcn, 6)).EntireRow.RowHeight = 16
         Columns(1).ColumnWidth = 3
         Columns(2).ColumnWidth = 15
         Columns(3).ColumnWidth = 10
